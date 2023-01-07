c49d89554e9a7d23a53d9a12e42a6c0c5fe0e3c6
Volzor <96282373+Volzor@users.noreply.github.com>
2023-01-06 21:56:56 -0800

    Exercise 01 finished

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,6 +1,19 @@
+
 window.addEventListener('keydown', (e) => {
-    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`); //selects the first audio element along with the element data-key that was fired fom the keydown event
+    const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
+    key.classList.add('playing');
 });
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => {
+    key.addEventListener('transitionend', removeTransition)
+})
